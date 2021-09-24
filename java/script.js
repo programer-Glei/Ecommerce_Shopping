3696a06fbaca3e245b03dd26bdfde17c7252ddf2
Gleibson Pereira <gleibson.santos05@gmail.com>
2021-09-24 20:04:16 -0300

    proximo comit

--- java/script.js
+++ java/script.js
@@ -4,4 +4,13 @@ $(document).ready(function(){
         $(this).toggleClass('fa-times')
         $('.navbar').toggleClass('nav-toggle')
     })
+
+    $(window).on('scroll load', function(){
+        $(this).removeClass('fa-times')
+        $('.navbar').removeClass('nav-toggle')
+
+        if($(window).scrollTop() > 68){
+            $('header .header-2').addClass('header-active')
+        }
+    })
 })
